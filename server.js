1ce204c30090b8e09261d56a9c7575911196b584
ashen017 <shen0000@email.unc.edu>
2022-09-05 11:29:44 -0400

    Pt7

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Require http module
 const http = require('http')
 // Require fs module
-const fs = require('Fs')
+const fs = require('fs')
 // Require minimist module (make sure you install this one via npm).
-const Args = Require('Minimist')(Process.Argv.Slice(2))
+const Args = require('minimist')(Process.Argv.Slice(2))
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.0
 Args.Port;
 // Define a const `port` using the argument from the command line. 
