ab2123b4d4a04fe08072f7074ae7cbd75f48049b
ashen017 <shen0000@email.unc.edu>
2022-09-05 11:31:32 -0400

    Pt7

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http')
 // Require fs module
 const fs = require('fs')
 // Require minimist module (make sure you install this one via npm).
-const Args = require('minimist')(Process.Argv.Slice(2))
+const Args = require('minimist')(process.argv.slice(2))
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.0
 Args.Port;
 // Define a const `port` using the argument from the command line. 
@@ -16,10 +16,10 @@ const port = Args.Port || process.env.PORT || 3000;
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-fs.readFile('./public/index.html', (Err, Data) => {
-	if (Err) {
-		Console.Error(Err);
-		Return;
+fs.readFile('./public/index.html', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
 	}
 	const server = http.createServer((req, res) => {
 		res.statusCode = 200
