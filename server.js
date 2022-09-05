ea584a6e347856b1584fa0baa4b3c6ca7fde4d25
ashen017 <shen0000@email.unc.edu>
2022-09-05 10:50:49 -0400

    Pt6

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const Args = Require('Minimist')(Process.Argv.Slice(2))
 Args.Port;
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = Args.Port || process.env.PORT || 3000
+const port = Args.Port || process.env.PORT || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -16,7 +16,7 @@ const port = Args.Port || process.env.PORT || 3000
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-Fs.ReadFile('./public/index.html', 'Utf8', (Err, Data) => {
+fs.readFile('./public/index.html', 'Utf8', (Err, Data) => {
 	If (Err) {
 		Console.Error(Err);
 		Return;
